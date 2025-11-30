c441f776674450477aa932d374c42623c944fc6f
asha <ashasharma08708@google.com>
2025-11-30 16:54:42 +0530

    code pushd 6

--- kubernetes-manifests/access-app.sh
+++ kubernetes-manifests/access-app.sh
@@ -2,12 +2,12 @@
 
 
 
-kubectl port-forward svc/gateway 7070:80 -n piggymetrics &
+kubectl port-forward svc/gateway 7070:80 -n piggymetricss &
 
 kubectl port-forward -n argocd service/argocd-server 8443:443 &
 
 kubectl port-forward svc/argo-server -n argo 2746:2746 &
 #kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
-
+#K0-sIzxqw8dzgN44
 #kubectl rollout restart deployment argocd-repo-server -n argocd
 #kubectl patch configmap argocd-cm -n argocd --type merge --patch '{"data":{"timeout.reconciliation":"10s","application.instanceLabelKey":"argocd.argoproj.io/instance"}}'
